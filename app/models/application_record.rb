dde3567a459eea9ac51be7523ac080080b64a208
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-22 09:23:26 +0300

    Added votes and vote count

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_184104) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_22_053510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contestants", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "number_of_votes", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number_of_votes", default: 0
     t.index ["user_id"], name: "index_contestants_on_user_id"
   end
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_184104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "vote_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
